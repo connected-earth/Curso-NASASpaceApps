94a0db5499937a2e86fedd36dea3e6a32b6c5c86
luc16 <luc.joffily.ribas@gmail.com>
2025-08-15 20:13:42 -0300

    Aula 2

---
+++ js/home.js
@@ -0,0 +1,30 @@
+// This code will run after the HTML document has been fully loaded
+document.addEventListener('DOMContentLoaded', () => {
+
+    const backgroundImages = [
+        'assets/bg1.jpg',
+        'assets/bg2.jpg',
+        'assets/bg3.jpg',
+        'assets/bg4.jpg',
+        'assets/bg5.jpg',
+        'assets/bg6.jpg',
+        'assets/bg7.jpg',
+    ];
+
+    const changeBgButton = document.getElementById('change-bg-btn');
+    const mainElement = document.querySelector('body.home-page main');
+
+    let currentImageIndex = 0;
+
+    changeBgButton.addEventListener('click', () => {
+        currentImageIndex++;
+
+        if (currentImageIndex >= backgroundImages.length) {
+            currentImageIndex = 0;
+        }
+
+        const newImageUrl = backgroundImages[currentImageIndex];
+
+        mainElement.style.backgroundImage = `url('${newImageUrl}')`;
+    });
+});
